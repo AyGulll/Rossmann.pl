e77a8fe85773201c572f8564ebadf1d070de3b17
AyGulll <abilovaygul@gmail@com>
2024-05-03 00:28:01 +0100

    login xpath

--- src/Rossmann/Login.java
+++ src/Rossmann/Login.java
@@ -2,6 +2,7 @@ package Rossmann;
 
 import Rossmann.Utility.BaseDriver;
 import Rossmann.Utility.MyFunction;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Login extends BaseDriver {
     public static WebDriver driver;
 
-
+    @Test
 
         public void LoginTest(){
     }
@@ -22,15 +23,17 @@ public class Login extends BaseDriver {
            MyFunction.wait(2);
            WebElement profil=driver.findElement(By.xpath("//span[normalize-space()='Profil']"));
            profil.click();
-           WebElement login=driver.findElement(By.xpath("//font[contains(text(),'Hesab yarat')]"));
-           login.click();
-           WebElement createaccount=driver.findElement(By.xpath("//font[contains(text(),'Hesab yarat')]"));
-           createaccount.click();
-           WebElement mailadress=driver.findElement(By.xpath("//input[@id='email']"));
+           WebElement cancel=driver.findElement(By.id("onetrust-close-btn-container"));
+           cancel.click();
+          // WebElement login=driver.findElement(By.xpath(""));
+         //  login.click();
+         //  WebElement createaccount=driver.findElement(By.xpath("//font[contains(text(),'Hesab yarat')]"));
+        //   createaccount.click();
+           WebElement mailadress=driver.findElement(By.id("login-user"));
            mailadress.sendKeys("abilovaygul@gmail.com");
-           WebElement password=driver.findElement(By.xpath("//input[@id='password']"));
+           WebElement password=driver.findElement(By.id("login-password"));
            password.sendKeys("YXt4_A@s-CYK!6g");
-           WebElement clickbutton=driver.findElement(By.xpath("//section[@class='register-info']//label[1]//span[1]"));
+           WebElement clickbutton=driver.findElement(By.id("id attribute is not available for this element"));
            clickbutton.click();
            WebElement clicckbutton2=driver.findElement(By.xpath("//label[2]//span[1]"));
            clicckbutton2.click();
--- src/Rossmann/Logout.java
+++ src/Rossmann/Logout.java
@@ -1,6 +1,7 @@
 package Rossmann;
 
 import Rossmann.Utility.BaseDriver;
+import Rossmann.Utility.MyFunction;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -13,6 +14,19 @@ public class Logout extends BaseDriver {
         driver.get("https://www.rossmann.pl/");
         WebElement login=driver.findElement(By.xpath(""));
         login.click();
+
+        WebElement mail=driver.findElement(By.xpath(""));
+        mail.sendKeys("abilovaygul@gmail.com");
+
+        WebElement password=driver.findElement(By.xpath(""));
+        password.sendKeys("YXt4_A@s-CYK!6g");
+        WebElement clickgiris=driver.findElement(By.id(""));
+        clickgiris.click();
+        MyFunction.wait(1);
+        WebElement logout=driver.findElement(By.xpath(""));
+        logout.click();
+
+        MyFunction.wait(1);
 
     }
 }
