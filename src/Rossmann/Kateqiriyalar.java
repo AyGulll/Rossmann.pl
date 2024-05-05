45a5e81b6c8f4ea6e68f4030427cd004bcd25027
AyGulll <abilovaygul@gmail@com>
2024-05-05 22:16:20 +0100

    Kateqoriyalar

---
+++ src/Rossmann/Kateqiriyalar.java
@@ -0,0 +1,15 @@
+package Rossmann;
+
+import Rossmann.Utility.BaseDriver;
+import org.junit.Test;
+
+public class Kateqiriyalar extends BaseDriver {
+
+
+
+    @Test
+    public void test(){
+
+    }
+
+}
